b7c2d25f844e9de6b69f014019ba921777f2d9df
Caio Rodrigues <caiordm08@gmail.com>
2024-10-07 12:03:30 -0300

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,51 @@
+<script>
+    import "../app.css";
+</script>
+
+<main class="w-full flex flex-col gap-4 items-center justify-center mb-4">
+    <slot />
+</main>
+
+<style>
+    @import url("https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap");
+    @import url("https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap");
+
+    :global(html) {
+        background-color: #ffffff;
+        opacity: 0.8;
+        background-image: linear-gradient(#f7f7f7 2px, transparent 2px),
+            linear-gradient(90deg, #f7f7f7 2px, transparent 2px),
+            linear-gradient(#f7f7f7 1px, transparent 1px),
+            linear-gradient(90deg, #f7f7f7 1px, #ffffff 1px);
+        background-size:
+            50px 50px,
+            50px 50px,
+            10px 10px,
+            10px 10px;
+        background-position:
+            -2px -2px,
+            -2px -2px,
+            -1px -1px,
+            -1px -1px;
+    }
+
+    :global(h2) {
+        font-size: 14pt;
+        font-weight: 700;
+        color: #701a75
+    }
+
+    :global(ul) {
+        display: flex;
+        flex-direction: column;
+        gap: 0.4rem;
+    }
+
+    :global(li) {
+        list-style: disc;
+    }
+
+    :global(strong) {
+        font-weight: 600;
+    }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,58 @@
+<script>
+    import { marked } from 'marked';
+
+    import { GoogleGenerativeAI } from "@google/generative-ai";
+    let gemini_api_key = import.meta.env.VITE_GEMINI_API_KEY;
+
+    let local = '';
+    let response = ''; 
+    let html = '';
+
+    const run = async () => {
+        const genAI = new GoogleGenerativeAI(gemini_api_key);
+        const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+        let prompt = `Estou pensando em ir para ${local}, liste para mim: os principais pontos turísticos, as melhores formas de ir e algumas dicas para não passar por perrengues.`
+
+        const result = await model.generateContent(prompt);
+        console.log(result.response);
+        response = result.response.text()
+        html = marked.parse(response);
+        console.log(html)
+    };
+</script>
+
+<div class="w-11/12 flex flex-col gap-1 items-center justify-center mt-4">
+    <h1 class="text-fuchsia-900 text-2xl font-black">ViageBem 🧳</h1>
+    <span class="text-slate-700 text-center"
+        >Diga pra onde você quer ir, e receba informações valiosas sobre seu
+        próximo destino! ✈️</span
+    >
+</div>
+
+<div class="w-11/12 flex flex-col gap-2">
+    <form
+        class="w-full flex justify-between bg-slate-100 rounded-2xl p-2 gap-1 border-2 border-slate-200 border-dashed"
+    >
+        <input
+            type="text"
+            bind:value={local}
+            placeholder="Para qual lugar você quer ir?"
+            name="local"
+            class="bg-transparent w-full text-md"
+        />
+        <button
+            id="button"
+            class="bg-fuchsia-900 text-white font-medium p-2 pl-4 pr-4 rounded-lg"
+            type="submit"
+            on:click={run}>Enviar</button
+        >
+    </form>
+    <!-- <Readme/> -->
+
+    {#if response}
+        <div class="w-full flex flex-col gap-3 p-4 pl-6 pr-6 bg-slate-100 border-2 border-slate-300 border-dashed rounded-3xl">
+            {@html html}
+        </div>
+    {/if}
+</div>
